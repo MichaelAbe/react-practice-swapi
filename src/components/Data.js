4e785bab90b3380f533afc3574bfddb5fc1c8c87
MichaelAbe <michael.k.abe@gmail.com>
2021-07-20 15:04:03 -0400

    fixed refresh issue

--- src/components/Data.js
+++ src/components/Data.js
@@ -4,6 +4,7 @@ export default function Data(props) {
     return (
         <div>
             <h3>{props.name}</h3>
+            <h4>{props.birth_year}</h4>
         </div>
     )
 }
--- src/components/DataContainer.js
+++ src/components/DataContainer.js
@@ -4,22 +4,27 @@ import Data from './Data'
 export default function DataContainer() {
 
     const [data, setData] = useState('')
-    
+    // const [loaded, setLoaded] = useState(false)
     useEffect(() => {
         fetch('https://swapi.dev/api/people')
         .then( resp => resp.json()
-        .then(data => setData(data)))
+        .then((data) => {
+            setData(data)
+            // setLoaded(true)
+        }))
     }, [])
 
     const mapChar = () => {
-        return data.results.map(person => < Data name =  {person.name} /> )
+        return data.results.map(person => < Data name =  {person.name} birth_year = {person.birth_year} /> )
     }
 
     return (
         <div>
             <h1>This is the data component</h1>
+            {console.log(data.results)}
             {console.log(data)}
-            { mapChar() }
+            {console.log(typeof data)}
+            { typeof data === 'object' ? mapChar() : null }
         </div>
     )
 }
