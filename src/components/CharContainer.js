f7e2d4db6a510eca20961ae41832647dded09b0a
MichaelAbe <michael.k.abe@gmail.com>
2021-08-05 18:02:12 -0400

    added fetch and mapChar

--- src/components/CharContainer.js
+++ src/components/CharContainer.js
@@ -1,8 +1,24 @@
 import React, {useState, useEffect} from 'react'
-import Data from './Data'
+import Char from './Char'
 
 
 export default function CharContainer() {
+
+    const [Char, setData] = useState('')
+    // const [loaded, setLoaded] = useState(false)
+    useEffect(() => {
+        fetch('https://swapi.dev/api/people')
+        .then( resp => resp.json()
+        .then((data) => {
+            setData(data)
+            // setLoaded(true)
+        }))
+    }, [])
+
+    const mapChar = () => {
+        return data.results.map(person => < Data name =  {person.name} birth_year = {person.birth_year} key = {person.url} /> )
+    }
+
     return (
         <div>
             
