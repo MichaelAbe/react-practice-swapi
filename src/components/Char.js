47d5144a57b539b410cf08356d05335a9488198d
MichaelAbe <michael.k.abe@gmail.com>
2021-08-05 18:04:59 -0400

    props in char

---
+++ src/components/Char.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Char() {
+    return (
+        <div>
+            <h3>{props.name}</h3>
+            <h4>{props.birth_year}</h4>
+        </div>
+    )
+}
