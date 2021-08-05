025fc3608ad721dbc03d437df96029efe1088872
MichaelAbe <michael.k.abe@gmail.com>
2021-08-05 18:04:00 -0400

    added results. changed to char

--- src/components/CharContainer.js
+++ src/components/CharContainer.js
@@ -4,24 +4,30 @@ import Char from './Char'
 
 export default function CharContainer() {
 
-    const [Char, setData] = useState('')
+    const [Char, setchar] = useState('')
     // const [loaded, setLoaded] = useState(false)
     useEffect(() => {
         fetch('https://swapi.dev/api/people')
         .then( resp => resp.json()
-        .then((data) => {
-            setData(data)
+        .then((char) => {
+            setchar(char)
             // setLoaded(true)
         }))
     }, [])
 
     const mapChar = () => {
-        return data.results.map(person => < Data name =  {person.name} birth_year = {person.birth_year} key = {person.url} /> )
+        return char.results.map(person => < Char name =  {person.name} birth_year = {person.birth_year} key = {person.url} /> )
     }
 
     return (
         <div>
-            
+              <div>
+            <h1>This is the char component</h1>
+            {console.log(char.results)}
+            {console.log(char)}
+            {console.log(typeof char)}
+            { typeof char === 'object' ? mapChar() : null }
+        </div>
         </div>
     )
 }
